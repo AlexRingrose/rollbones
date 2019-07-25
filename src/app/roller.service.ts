0b0c123d8b7e82d8fa3e02278780b4dc1f8c7ab0
AlexRingrose <ringrose.alex@gmail.com>
2019-07-24 23:06:02 -0500

    building roll parser

--- src/app/roller.service.ts
+++ src/app/roller.service.ts
@@ -4,11 +4,23 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RollerService {
-  constructor() {
+  tString;
+  constructor () {
+    let tInput = '4d6'
+  }
+  /*
+    My regex filter:
+    https://regexr.com/4i2nq
+    /((\d*)?d(\d+)(([+-/*]\d+(?!d))|(k\d+)|([+-/*](\d*)?d\d+))?){1}/g
+  */
+
+  rollParser (input:string) {
+    let num: number, die: number, bonus: number;
+    let keep: number, drop: number;
   }
 
   rollSet ( num: number, die: number ) {
-    let set: Array<number> = [];
+    let set: Array<any> = [];
     for ( let i = 0; i < num; i++ ){
       set[ i ] = this.rollDie( die );
     }
@@ -16,6 +28,9 @@ export class RollerService {
   }
 
   rollDie ( die: number ) {
-    return Math.floor(Math.random() * ( die ) + 1);
+    return  {
+      die: "d" + die,
+      roll: Math.floor( Math.random() * ( die ) + 1 )
+    };
   }
 }
