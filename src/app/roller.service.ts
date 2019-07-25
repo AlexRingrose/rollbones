9ba4b1ac84b4ce60cb18d125bcfac1fd4ac7f42d
AlexRingrose <ringrose.alex@gmail.com>
2019-07-24 21:49:32 -0500

    roller basics

--- src/app/roller.service.ts
+++ src/app/roller.service.ts
@@ -1,24 +1,21 @@
 import { Injectable } from '@angular/core';
-import { xorshift } from 'xorshift/xorshift';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RollerService {
-
   constructor() {
-
   }
 
-  private rollSet ( num: number, die: number ) {
-    let set: Array<number>;
+  rollSet ( num: number, die: number ) {
+    let set: Array<number> = [];
     for ( let i = 0; i < num; i++ ){
-      set[ 0 ] = this.rollDie( die );
+      set[ i ] = this.rollDie( die );
     }
     return set;
   }
 
-  private rollDie ( die:number ) {
-    return Math.floor( 1 + xorshift.random() * ( die - 1 ) );
+  rollDie ( die: number ) {
+    return Math.floor(Math.random() * ( die ) + 1);
   }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RollerService } from '../roller.service'
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor (public roller_:RollerService) {
+
+  }
+
+  test() {
+    console.log( this.roller_.rollSet( 6, 20 ) );
+  }
 
 }
