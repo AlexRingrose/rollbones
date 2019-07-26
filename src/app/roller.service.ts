cc29f81e05bd4602246b3cd4ef09fcccb31c26cf
AlexRingrose <ringrose.alex@gmail.com>
2019-07-26 18:38:34 -0500

    functional parse roller for  #, d#, #d#

--- src/app/roller.service.ts
+++ src/app/roller.service.ts
@@ -1,73 +1,92 @@
 import { Injectable, SystemJsNgModuleLoader } from '@angular/core';
 import { strict } from 'assert';
 
-@Injectable({
+@Injectable( {
   providedIn: 'root'
-})
+} )
 export class RollerService {
-  tString;
   constructor () {
 
   }
   /*
     My regex filter:
     https://regexr.com/4i2nq
-    /((\d*)?d(\d+)(([+-/*]\d+(?!d))|(k\d+)|([+-/*](\d*)?d\d+))?){1}/g
+    /((\d*)?d(\d+)(([+-/*]\d+(?!d))|(k\d+)|([+-/*](\d*)?d\d+))?(([+-/*]\d+(?!d))|(\d+)|([+-/*](\d*)?d\d+))?(([+-/*]\d+(?!d))|(\d+)|([+-/*](\d*)?d\d+))?){1}/g
   */
 
   //TODO: add support for numeric and die adding e.g. 2d6 + d6 + 2
 
   rollParser ( input: string ) {
     let matches: Array<string>;
-    //match dice rolls in traditional format with modifiers
+    let rollSet: Array<any> = [];
+    //match dice rolls in traditional format with modifiers up to 4 'terms'
     matches = input.match(
-      /((\d*)?d(\d+)(([+-/*]\d+(?!d))|(k\d+)|([+-/*](\d*)?d\d+))?){1}/g
+      /((\d*)?d(\d+)(([+-/*]\d+(?!d))|(k\d+)|([+-/*](\d*)?d\d+))?(([+-/*]\d+(?!d))|(\d+)|([+-/*](\d*)?d\d+))?(([+-/*]\d+(?!d))|(\d+)|([+-/*](\d*)?d\d+))?){1}/g
     );
     console.log( matches );
 
-    //TODO: For single match, loop or functionify
+    //TODO: For single match, loop or functionify for multi matches
 
-    //find which delimitter and store it
     //TODO:~ K not dealt with yet
-    //split on +-/*
-    //split on d
-    let num: number, die: number, bonus: number, delim: string;
-    const temp = matches[ 0 ];
-    delim = temp[ temp.search( /[+-/*]|k/ ) ];
-    let splitOnDelim = temp.split( /[+-/*]|k/ );
-    console.log( "Temp, Delim, splitOnDelim",temp, delim, splitOnDelim );
+    const singleMatch = matches[ 0 ];
+    const delims = singleMatch.match( /[+-/*]|k/g );
+    let splitOnDelim = singleMatch.split( /[+-/*]|k/g );
+    console.log( "This match: ", singleMatch );
 
-    //Base Die
-    //fixed index splitOnDelim, change if supporting numeric and die adding
-    if ( (splitOnDelim[ 0 ].search( /^d/ ) ) >= 0) {
-      //console.log("no leading num E.G. d8")
-      let splitOnBaseDie = splitOnDelim[ 0 ].replace( /d/, "" );
-      num = 1;
-      die = Number( splitOnBaseDie );
-      console.log(splitOnBaseDie, num, die)
-    } else {
-      //console.log("leading num")
-      let splitOnBaseDie = splitOnDelim[ 0 ].split( /d/ )
-      num = Number( splitOnBaseDie[ 0 ] );
-      die = Number( splitOnBaseDie[ 1 ] );
+    for ( let i = 0; i < splitOnDelim.length; i++ ) {
+      let num: number, die: number;
+      let splitOnSecondary = splitOnDelim[ i ];
+      if ( delims[ i - 1 ] === 'k' ) {
+        //TODO: Keep <splitOnDelim[i] highest dice>
+
+      } else if ( ( splitOnSecondary.match( /(d)/ ) ) === null ) {
+        console.log( 'Roll Type: #' );
+        let res = {};
+        Object.assign( res, {
+          rolls: [], die: 0,
+          func: delims[ i - 1 ], mod: splitOnSecondary
+        } );
+        rollSet[ i ] = res;
+
+      } else if ( ( splitOnSecondary.match( /^d/g ) ) !== null ) {
+        console.log( 'Roll Type: d#' );
+        let splitOnDie = splitOnSecondary.replace( /d/, "" );
+        num = 1;
+        die = Number( splitOnDie );
+        let tres = this.roll( num, die );
+        let res = {};
+        Object.assign( res, {
+          rolls: tres.rolls, die: tres.die,
+          func: delims[ i - 1 ], mod: 0
+        } );
+        rollSet[ i ] = res;
+
+      } else {
+        console.log( 'Roll Type: #d#' );
+        let splitOnDie = splitOnSecondary.split( /d/ )
+        num = Number( splitOnDie[ 0 ] );
+        die = Number( splitOnDie[ 1 ] );
+        let tres = this.roll( num, die );
+        let res = {};
+        Object.assign( res, {
+          rolls: tres.rolls, die: tres.die,
+          func: delims[ i - 1 ], mod: 0
+        } );
+        rollSet[ i ] = res;
+      }
     }
-    let baseDie = this.roll( num, die );
-
-    //Secondary
-    let splitOnSecondary = splitOnDelim[1]
-
-    console.log( 'roll base die:' , baseDie) ;
+    console.log( 'roll Set:', rollSet );
   }
 
   roll ( num: number, die: number ) {
     let set: Array<any> = [];
-    for ( let i = 0; i < num; i++ ){
+    for ( let i = 0; i < num; i++ ) {
       set[ i ] = this.rollDie( die );
     }
     return { rolls: set, die: die };
   }
 
   private rollDie ( die: number ) {
-    return  Math.floor( Math.random() * ( die ) + 1 )
+    return Math.floor( Math.random() * ( die ) + 1 )
   }
 }
