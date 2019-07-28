4c12e1153d782b1dd300d68a5b0f6231b7252d2d
AlexRingrose <ringrose.alex@gmail.com>
2019-07-28 16:20:40 -0500

    testing tools corrected for new name

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,19 +1,18 @@
 import { Component } from '@angular/core';
 import { RollerService } from '../roller.service'
 
-@Component({
+@Component( {
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
-})
+  styleUrls: [ 'tab1.page.scss' ]
+} )
 export class Tab1Page {
   inputString;
-  constructor (public roller_:RollerService) {
+  constructor ( public roller_: RollerService ) {
 
   }
 
-  test() {
-    console.log( this.roller_.roll( 6, 20 ) );
+  test () {
     this.roller_.rollParser( this.inputString );
   }
 
