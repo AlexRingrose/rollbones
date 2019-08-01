3eb980f99ec386f561e552cb4d3c8f8e1d39d061
AlexRingrose <ringrose.alex@gmail.com>
2019-08-01 15:56:06 -0500

    simple interface

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,12 +8,15 @@ import { RollerService } from '../roller.service'
 } )
 export class Tab1Page {
   inputString;
+  rollHistory;
   constructor ( public roller_: RollerService ) {
-
+    this.rollHistory = [];
   }
 
   test () {
     this.roller_.rollParser( this.inputString );
+    this.rollHistory = this.roller_.getHistory();
+    console.log( "RollHistory: ", this.rollHistory );
   }
 
 }
